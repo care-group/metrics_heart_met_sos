06f42a5aa55c92d14ea0a88f6f94c6a13c5cb1f2
Ronnie Smith <ronniesmith@outlook.com>
2023-05-19 17:26:03 +0100

    Added person searchign

--- metrics_controller/src/main.py
+++ metrics_controller/src/main.py
@@ -8,22 +8,26 @@ from enum import Enum
 # internal classes
 from log import Log
 from speak import Speak
+from object_to_tf import ObjectToTF
 
 # standard messages
 from std_msgs.msg import String
 from hsrb_interface import Robot, exceptions
 
 # custom messages
-# none
+from ronsm_messages.msg import dm_intent
 
 # constants and parameters
-# none
+MAX_USER_INPUT_RETRIES = 3
+MAX_USER_INPUT_WAIT = 30
+MAX_PERSON_SEARCH_TRIES = 6
 
 class State(Enum):
     IDLE = 0
     APPROACH_WAIT = 1
     APPROACH_AUTO = 2
     OFFER_BRING = 3
+    FAIL = 99
 
 class Main():
     def __init__(self):
@@ -35,11 +39,13 @@ class Main():
         # set up ROS
         rospy.init_node('metrics_controller')
 
+        self.rps_sub_intent_bus = rospy.Subscriber('/metrics_rasa_core/intent_bus', dm_intent, callback=self.ros_callback_intent_bus)
         self.ros_sub_start_signal = rospy.Subscriber('/metrics_controller/start_signal', String, callback=self.ros_callback_start_signal)
         self.ros_pub_service_listen_once = rospy.Publisher('/metrics_rasa/listen_once', String, queue_size=10)
 
         # set up classes
         self.speak = Speak()
+        self.object_to_transform = ObjectToTF()
 
         # set up HSR
         self.robot = Robot()
@@ -48,7 +54,7 @@ class Main():
             self.base = self.robot.try_get('omni_base')
             self.body = self.robot.try_get('whole_body')
             self.grip = self.robot.try_get('gripper')
-        except:
+        except exceptions.ResourceNotFoundError:
             self.logger.log_warn('Unable to get one or more handles for HSR resources. Another process may be using them or the robot is in an error state.')
             self.speak.request('Controller failed to start, please check console.')
             exit(0)
@@ -56,6 +62,7 @@ class Main():
         # instance variables
         self.state = State.IDLE
         self.start = False
+        self.intent = None
 
         # ready
         self.logger.log_great('Ready.')
@@ -78,6 +85,8 @@ class Main():
             self.state_approach_auto()
         elif self.state == State.OFFER_BRING:
             self.state_offer_bring()
+        elif self.state == State.FAIL:
+            self.state_fail()
 
     # states
 
@@ -92,24 +101,102 @@ class Main():
         pass
 
     def state_approach_auto(self):
-        # approach
-        self.state = State.OFFER_BRING
+        self.speak.request('I am searching for a person!')
+
+        tries = 0
+        success = False
+
+        while (tries < MAX_PERSON_SEARCH_TRIES):
+            success = self.object_to_transform.check_for_object('person')
+            if success:
+                break
+            self.base.go_rel(yaw=1.0)
+            self.body.move_to_joint_positions({'head_tilt_joint': 0.0})
+            tries = tries + 1
+
+        if not success:
+            self.state = State.FAIL
+            return
+
+        self.logger.log('Found a person.')
+        self.speak.request('Oh, there you are!')
+
+        self.state = State.IDLE
+        # self.state = State.OFFER_BRING
 
     def state_offer_bring(self):
         self.speak.request('Sorry to interrupt, but I was wondering if there is anything I can bring you at the moment?')
-        self.service_listen_once()
+        
+        self.intent = None
+        success = self.service_listen_and_wait('Sorry, I did not understand. Is there anything I can get you?', ['intent_item'])
+
+        if not success:
+            self.state = State.FAIL
+            return
+
+        item = self.intent.args[0]
+        say = 'Ok, I can bring you the ' + item + '. Where can I find that?'
+        self.speak.request(say)
+
+        self.intent = None
+        success = self.service_listen_and_wait('Sorry, I did not understand. Where is the item located', ['intent_location'])
+
+        if not success:
+            self.state = State.FAIL
+            return
+
+        location = self.intent.args[0]
+        say = 'Ok, I will fetch you the ' + item + ' from the ' + location + '.'
+        self.speak.request(say)
+        
+        self.state = State.IDLE
+
+    def state_fail(self):
+        self.speak.request('Sorry, I was not able to understand what you wanted. I will now reset.')
         self.state = State.IDLE
 
     # service calls
-    def service_listen_once(self):
+
+    def service_listen_and_wait(self, repeat_message, valid_intents):
         msg = String()
         msg.data = ''
-        self.ros_pub_service_listen_once.publish(msg)
+
+        valid = False
+        tries = 0
+        while (tries < MAX_USER_INPUT_RETRIES) and (not valid):
+            if tries > 0:
+                self.speak.request(repeat_message)
+
+            self.ros_pub_service_listen_once.publish(msg)
+
+            wait = 0
+            response = False
+            while (wait < MAX_USER_INPUT_WAIT) and (not response):
+                self.logger.log('Waiting for user input...')
+
+                if self.intent != None:
+                    response = True
+
+                rospy.sleep(1)
+                wait = wait + 1
+
+            if self.intent != None:
+                if self.intent.intent in valid_intents:
+                    valid = True
+
+            tries = tries + 1
+
+        return valid
 
     # callbacks
 
     def ros_callback_start_signal(self, msg):
         self.start = True
+
+    def ros_callback_intent_bus(self, msg):
+        self.intent = msg
+        log = 'Received intent: ' + msg.intent
+        self.logger.log(log)
 
 if __name__ == '__main__':
     m = Main()
---
+++ metrics_controller/src/object_to_tf.py
@@ -0,0 +1,137 @@
+#!/usr/bin/env python3
+
+# standard libraries
+import rospy
+import ros_numpy
+import tf2_ros
+import tf_conversions
+
+# internal classes
+from log import Log
+
+# standard messages
+from geometry_msgs.msg import TransformStamped
+from std_msgs.msg import Int32
+from sensor_msgs.msg import PointCloud2, Image
+from darknet_ros_msgs.msg import BoundingBoxes
+
+# custom messages
+# none
+
+# constants and parameters
+MAX_WAIT_BBS = 5
+MAX_WAIT_DEPTH = 20
+
+class ObjectToTF():
+    def __init__(self):
+        # set up logger
+        self.id = 'object_to_tf'
+        self.logger = Log(self.id)
+
+        # set up ROS
+        self.ros_sub_bounding_boxes = rospy.Subscriber('/darknet_ros/bounding_boxes', BoundingBoxes, self.ros_callback_bounding_boxes)
+        self.ros_sub_rgbd = rospy.Subscriber('/hsrb/head_rgbd_sensor/depth_registered/rectified_points', PointCloud2, self.ros_callback_rgbd)
+        self.ros_sub_rgbd_raw = rospy.Subscriber('/hsrb/head_rgbd_sensor/depth_registered/image_rect_raw', Image, self.ros_callback_rgbd_raw)
+
+        self.ros_pub_depth = rospy.Publisher('/metrics_controller/object_to_tf/depth_out', PointCloud2, queue_size=10)
+        self.ros_pub_centre_depth = rospy.Publisher('/metrics_controller/object_to_tf/depth_centre_out', Int32, queue_size=10)
+        self.ros_pub_tf = tf2_ros.StaticTransformBroadcaster()
+
+        # instance variables
+        self.bounding_boxes = None
+        self.depth = None
+        self.image = None
+
+        # ready
+        self.logger.log_great('Ready.')
+
+    # core logic
+
+    def check_for_object(self, target):
+        self.bounding_boxes = None
+
+        waits = 0
+        while(self.bounding_boxes is None):
+            self.logger.log('Waiting for bounding boxes...')
+            rospy.sleep(1.0)
+            waits = waits + 1
+            if waits == MAX_WAIT_BBS:
+                self.logger.log_warn('No bounding boxes were received on time. Check that DarknetROS is running and the objects are visible.')
+                return False
+
+        for bounding_box in self.bounding_boxes:
+            if bounding_box.Class == target:
+                return True
+
+        return False
+
+
+    def object_to_tf(self, target):
+        waits = 0
+        while(self.depth is None):
+            self.logger.log('Waiting for depth image...')
+            rospy.sleep(1.0)
+            waits = waits + 1
+            if waits == MAX_WAIT_DEPTH:
+                self.logger.log_warn('No depth images were received on time. Ensure that the robot is not under unusually high CPU load, or increase wait time.')
+                return False
+
+        for bounding_box in self.bounding_boxes:
+            if bounding_box.Class == target:
+                self.segment(bounding_box)
+                return True
+                
+        return False
+
+    def segment(self, bb):
+        subsample = self.depth[bb.ymin:bb.ymax, bb.xmin:bb.xmax]
+        print('Depth shape:', self.depth.shape)
+        print('Subsample shape:', subsample.shape)
+
+        msg = ros_numpy.msgify(PointCloud2, subsample)
+
+        self.ros_pub_depth.publish(msg)
+
+        flat_subsample = subsample.flatten()
+        sumd = 0
+        for sample in flat_subsample:
+            sumd = sumd + sample[2]
+        avg = sumd / len(flat_subsample)
+        row = subsample.shape[0] / 2
+        col = subsample.shape[1] / 2
+        row = int(round(row, 0))
+        col = int(round(col, 0))
+        print(row, col)
+        point = subsample[row, col]
+        print('Centre:', point, avg)
+
+        t = TransformStamped()
+
+        print(point)
+
+        t.header.stamp = rospy.Time.now()
+        t.header.frame_id = 'head_rgbd_sensor_link'
+        t.child_frame_id = 'object_of_interest'
+        t.transform.translation.x = point[0]
+        t.transform.translation.y = point[1]
+        t.transform.translation.z = point[2]
+        q = tf_conversions.transformations.quaternion_from_euler(0, 0, 0)
+        t.transform.rotation.x = q[0]
+        t.transform.rotation.y = q[1]
+        t.transform.rotation.z = q[2]
+        t.transform.rotation.w = q[3]
+
+        self.ros_pub_tf.sendTransform(t)
+
+    # callbacks
+
+    def ros_callback_bounding_boxes(self, msg):
+        self.bounding_boxes = msg.bounding_boxes
+
+    def ros_callback_rgbd(self, msg):
+        data = ros_numpy.numpify(msg)
+        self.depth = data
+
+    def ros_callback_rgbd_raw(self, msg):
+        data = ros_numpy.numpify(msg)
+        self.image = data
